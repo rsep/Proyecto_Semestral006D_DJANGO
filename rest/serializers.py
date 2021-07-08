29498bc5ac68a3ac0ac0955f88d59bfde1b117ac
rsep <78937557+rsep@users.noreply.github.com>
2021-07-07 23:01:34 -0400

    Delete rest directory
    Api con errores y fuera del proyecto. Se hizo versión corregida e integrada en proyecto.

--- rest/serializers.py
+++
@@ -1,9 +0,0 @@
-from django.db.models import fields
-from Proyecto_Semestral006D_DJANGO.core.models import Obra
-from django.db.models.base import Model
-from rest_framework import serializers
-from core.models import Obra
-class ObraSerializer(Serializers.ModelSerializers):
-    class Meta:
-        model: Obra
-        fields = ['id_obra','precio','titulo','año','historia','descripcion','tecnica','imagen_obra','autor']
